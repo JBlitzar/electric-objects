50aa4eacc104358b8680f14726fb2f31fc64e15b
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-12-14 21:02:58 -0800

    push?

--- app/src/main/java/com/dan/eo1/MainActivity.java
+++ app/src/main/java/com/dan/eo1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.dan.eo1;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -10,30 +12,45 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        WebView webView = new WebView(this);
+        WebView webView = findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
-        setContentView(webView);
-
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://jblitzar.github.io/electric-objects");
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_S) {
-            Intent intent = new Intent(android.provider.Settings.ACTION_SETTINGS);
-            startActivity(intent);
-            return true;
-        }
-        if (keyCode == KeyEvent.KEYCODE_H) {
-            Intent homeIntent = new Intent(Intent.ACTION_MAIN);
-            homeIntent.addCategory(Intent.CATEGORY_HOME);
-            homeIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(homeIntent);
-            return true;
-        }
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            Log.d("KeyPress", "Key Code: " + keyCode);
+            switch (event.getKeyCode()) {
+                case KeyEvent.KEYCODE_C:
+                    // Open settings when "C" is pressed
+                    openSettings();
+                    return true;
 
+                case KeyEvent.KEYCODE_H:
+                    // Go to the home screen when "H" is pressed
+                    goToHomeScreen();
+                    return true;
+
+                default:
+                    return super.onKeyDown(keyCode, event);
+            }
+        }
         return super.onKeyDown(keyCode, event);
+    }
+
+    private void openSettings() {
+        Intent intent = new Intent(Settings.ACTION_SETTINGS);
+        startActivity(intent);
+    }
+
+    private void goToHomeScreen() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
