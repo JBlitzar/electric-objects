4a030fb48cb86fea965b9e3724e7e885d21a50ff
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-12-14 20:30:32 -0800

    S for settings

--- app/src/main/java/com/dan/eo1/MainActivity.java
+++ app/src/main/java/com/dan/eo1/MainActivity.java
@@ -1,16 +1,32 @@
 package com.dan.eo1;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         WebView webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient());
         setContentView(webView);
+
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("https://jblitzar.github.io/electric-objects/");
+        webView.loadUrl("https://jblitzar.github.io/electric-objects");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_S) {
+            Intent intent = new Intent(android.provider.Settings.ACTION_SETTINGS);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
